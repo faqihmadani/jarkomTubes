977a6275cca60ba08a97be0fd4b95f919e632a49
faqihmadani <ubuntu-faqih@email.comgit config --global user.email ubuntu-faqih@email.com>
2022-06-08 16:30:09 +0700

    clo 1 finished

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import Node
@@ -12,26 +10,35 @@ import os
 
 
 class MyTopo (Topo):
-    def __init__(self, **opts):
+	def __init__(self, **opts):
+		Topo.__init__(self, **opts)
 
-    Topo.__init__(self, **opts)
+    		# Membuat objek host
+		H1 = self.addHost('H1', ip='10.0.0.1/24')
+		H2 = self.addHost('H2', ip='10.0.0.2/24')
 
-    # Membuat objek host
-    H1 = self.addHost('H1', ip='10.0.0.1/24')
-    H2 = self.addHost('H2', ip='10.0.0.2/24')
+    		# Membuat objek router
+		R1 = self.addHost('R1')
+		R2 = self.addHost('R2')
+		R3 = self.addHost('R3')
+		R4 = self.addHost('R4')
 
-    # Membuat objek router
-    R1 = self.addHost('R1')
-    R2 = self.addHost('R2')
-    R3 = self.addHost('R3')
-    R4 = self.addHost('R4')
+		# Membuat Link
+		self.addLink(R1, R4, bw=1)
+		self.addLink(R1, R3, bw=0.5)
+		self.addLink(R2, R3, bw=1)
+		self.addLink(R2, R4, bw=0.5)
+		self.addLink(H1, R1, bw=1)
+		self.addLink(H1, R2, bw=1)
+		self.addLink(H2, R3, bw=1)
+		self.addLink(H2, R4, bw=1)
 
 
 def runTopo():
-    # Memastikan mininet bersih dari cache sebelumnya
-    os.system('mn -c')
+	# Memastikan mininet bersih dari cache sebelumnya
+	os.system('mn -c')
 
-    # Membangun Topologi
-    topo = MyTopo()
-    net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
-    net.start()
+	# Membangun Topologi
+	topo = MyTopo()
+	net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
+	net.start()
