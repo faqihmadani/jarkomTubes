31ce253fe90ba4d8c51e644674a06cda10d464e2
faqihmadani <ubuntu-faqih@email.comgit config --global user.email ubuntu-faqih@email.com>
2022-06-13 08:15:56 +0700

    clo4 finished

--- main.py
+++ main.py
@@ -39,6 +39,32 @@ class MyTopo (Topo):
 		self.addLink(r2, r3, cls=TCLink, bw=1, **linkopt)
 		self.addLink(r1, r4, cls=TCLink, bw=1, **linkopt)
 		
+class MyTopoBuffer (Topo):
+	def __init__(self, queue_size, **opts):
+		Topo.__init__(self, **opts)
+
+		linkopt = {'delay' : '0ms', 'loss' : 0}
+
+		# Membuat objek router
+		r1 = self.addHost('r1')
+		r2 = self.addHost('r2')
+		r3 = self.addHost('r3')
+		r4 = self.addHost('r4')
+
+    		# Membuat objek host
+		h1 = self.addHost('h1', cls=Host)
+		h2 = self.addHost('h2', cls=Host)
+
+    		
+		# Membuat Link
+		self.addLink(h1, r1, max_queue_size=queue_size, use_htb=True, cls=TCLink, bw=1, **linkopt)
+		self.addLink(h1, r2, max_queue_size=queue_size, use_htb=True, cls=TCLink, bw=1, **linkopt)
+		self.addLink(h2, r3, max_queue_size=queue_size, use_htb=True, cls=TCLink, bw=1, **linkopt)
+		self.addLink(h2, r4, max_queue_size=queue_size, use_htb=True, cls=TCLink, bw=1, **linkopt)
+		self.addLink(r1, r3, max_queue_size=queue_size, use_htb=True, cls=TCLink, bw=0.5, **linkopt)
+		self.addLink(r2, r4, max_queue_size=queue_size, use_htb=True, cls=TCLink, bw=0.5, **linkopt)
+		self.addLink(r2, r3, max_queue_size=queue_size, use_htb=True, cls=TCLink, bw=1, **linkopt)
+		self.addLink(r1, r4, max_queue_size=queue_size, use_htb=True, cls=TCLink, bw=1, **linkopt)
 		
 
 def runTopo():
@@ -219,7 +245,7 @@ def runTopo():
 	print("\nRouting telah selesai\n")
 
 	#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
-	
+	'''
 	#CLO 3 TCP
 	#setting h2 sebagai server
 	h2.cmd("iperf -s &")
@@ -234,9 +260,180 @@ def runTopo():
 	h1.cmdPrint("tcpdump -r result.pcap")
 	
 	print("Proses tcpdump telah selesai")
+	'''
+	
+	CLI(net)
+	net.stop()
+	
+def runTopoBuffer():
+	print("\nUji Buffer pada Topologi\n")
+	ukuran_buffer = int(input("Masukkan ukuran buffer "))
+
+	# Memastikan mininet bersih dari cache sebelumnya
+	os.system('mn -c')
+
+	# CLO 1 Membangun Topologi
+	topo = MyTopoBuffer(ukuran_buffer)
+	net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
+	net.start()
+	#net.build()
+	
+	#Memasukkan objek host pada variabel
+	h1,h2,r1,r2,r3,r4 = net.get('h1', 'h2', 'r1', 'r2', 'r3', 'r4')
+	
+	#Setting H1
+	h1.cmd("ifconfig h1-eth0 0")
+	h1.cmd("ifconfig h1-eth1 0")
+	
+	#Setting H2
+	h1.cmd("ifconfig h2-eth0 0")
+	h1.cmd("ifconfig h2-eth1 0")
+	
+	#Setting R1
+	r1.cmd("ifconfig r1-eth0 0")
+	r1.cmd("ifconfig r1-eth1 0")
+	r1.cmd("ifconfig r1-eth2 0")
+	
+	#Setting R2
+	r2.cmd("ifconfig r2-eth0 0")
+	r2.cmd("ifconfig r2-eth1 0")
+	r2.cmd("ifconfig r2-eth2 0")
+	
+	#Setting R3
+	r3.cmd("ifconfig r3-eth0 0")
+	r3.cmd("ifconfig r3-eth1 0")
+	r3.cmd("ifconfig r3-eth2 0")
+	
+	#Setting R4
+	r4.cmd("ifconfig r4-eth0 0")
+	r4.cmd("ifconfig r4-eth1 0")
+	r4.cmd("ifconfig r4-eth2 0")
+	
+	#H1
+	#ke R1
+	h1.cmd("ifconfig h1-eth0 194.169.1.1/30")
+	#h1.cmd("ip route add default gw 194.169.1.2 h1-eth0")
+	#ke R2
+	h1.cmd("ifconfig h1-eth1 194.169.6.1/30")
+	#h1.cmd("ip route add default gw 194.169.6.2 h1-eth1")
+	
+	#H2
+	#ke R3
+	h2.cmd("ifconfig h2-eth0 194.169.3.1/30")
+	#h2.cmd("ip route add default gw 194.169.3.2 h2-eth0")
+	#ke R4
+	h2.cmd("ifconfig h2-eth1 194.169.4.1/30")
+	#h2.cmd("ip route add default gw 194.169.4.2 h2-eth1")
+	
+	#R1
+	#ke H1
+	r1.cmd("ifconfig r1-eth0 194.169.1.2/30")
+	#ke R3
+	r1.cmd("ifconfig r1-eth1 194.169.2.1/30")
+	#ke R4
+	r1.cmd("ifconfig r1-eth2 194.169.7.1/30")
+	
+	#R2
+	#ke H1
+	r2.cmd("ifconfig r2-eth0 194.169.6.2/30")
+	#ke R4
+	r2.cmd("ifconfig r2-eth1 194.169.5.1/30")
+	#ke R3
+	r2.cmd("ifconfig r2-eth2 194.169.8.1/30")
+	
+	#R3
+	#ke H2
+	r3.cmd("ifconfig r3-eth0 194.169.3.2/30")
+	#ke R1	
+	r3.cmd("ifconfig r3-eth1 194.169.2.2/30")
+	#ke R2
+	r3.cmd("ifconfig r3-eth2 194.169.8.2/30")
+	
+	#R4
+	#ke H2
+	r4.cmd("ifconfig r4-eth0 194.169.4.2/30")
+	#ke R2
+	r4.cmd("ifconfig r4-eth1 194.169.5.2/30")
+	#ke R1
+	r4.cmd("ifconfig r4-eth2 194.169.7.2/30")
+	
+	#CLO 2 Routing
+	
+	#Routing H1
+	h1.cmd("ip rule add from 194.169.1.1 table 1")
+	h1.cmd("ip rule add from 194.169.6.1 table 2")
+	#table 1
+	h1.cmd("ip route add 194.169.1.0/30 dev h1-eth0 scope link table 1")
+	h1.cmd("ip route add default via 194.169.1.2 dev h1-eth0 table 1")
+	h1.cmd("ip route add default gw 194.169.1.2 dev h1-eth0")
+	h1.cmd("ip route add default scope global nexthop via 194.169.1.2 dev h1-eth0")
+	#table 2
+	h1.cmd("ip route add 194.169.6.0/30 dev h1-eth1 scope link table 2")
+	h1.cmd("ip route add default via 194.169.6.2 dev h1-eth1 table 2")
+	h1.cmd("ip route add default gw 194.169.6.2 dev h1-eth1")
+	h1.cmd("ip route add default scope global nexthop via 194.169.6.2 dev h1-eth1")
+	
+	#Routing H2
+	h2.cmd("ip rule add from 194.169.3.1 table 1")
+	h2.cmd("ip rule add from 194.169.4.1 table 2")
+	#table 1
+	h2.cmd("ip route add 194.169.3.0/30 dev h2-eth0 scope link table 1")
+	h2.cmd("ip route add default via 194.169.3.2 dev h2-eth0 table 1")
+	h2.cmd("ip route add default gw 194.169.3.2 dev h2-eth0")
+	h2.cmd("ip route add default scope global nexthop via 194.169.3.2 dev h2-eth0")
+	#table 2
+	h2.cmd("ip route add 194.169.4.0/30 dev h2-eth1 scope link table 2")
+	h2.cmd("ip route add default via 194.169.4.2 dev h2-eth1 table 2")
+	h2.cmd("ip route add default gw 194.169.4.2 dev h2-eth1")
+	h2.cmd("ip route add default scope global nexthop via 194.169.4.2 dev h2-eth1")
+	
+	
+	#Routing R1
+	r1.cmd("sysctl net.ipv4.ip_forward=1")
+	r1.cmd("route add -net 194.169.3.0/30 gw 194.169.2.2")
+	r1.cmd("route add -net 194.169.4.0/30 gw 194.169.7.2")
+	r1.cmd("route add -net 194.169.6.0/30 gw 194.169.7.2")
+	r1.cmd("route add -net 194.169.8.0/30 gw 194.169.2.2")
+	r1.cmd("route add -net 194.169.5.0/30 gw 194.169.7.2")
+	
+	#Routing R2
+	r2.cmd("sysctl net.ipv4.ip_forward=1")
+	r2.cmd("route add -net 194.169.1.0/30 gw 194.169.8.2")
+	r2.cmd("route add -net 194.169.3.0/30 gw 194.169.8.2")
+	r2.cmd("route add -net 194.169.4.0/30 gw 194.169.5.2")
+	r2.cmd("route add -net 194.169.7.0/30 gw 194.169.5.2")
+	r2.cmd("route add -net 194.169.2.0/30 gw 194.169.8.2")
+	
+	#Routing R3
+	r3.cmd("sysctl net.ipv4.ip_forward=1")
+	r3.cmd("route add -net 194.169.1.0/30 gw 194.169.2.1")
+	r3.cmd("route add -net 194.169.6.0/30 gw 194.169.8.1")
+	r3.cmd("route add -net 194.169.4.0/30 gw 194.169.8.1")
+	r3.cmd("route add -net 194.169.7.0/30 gw 194.169.2.1")
+	r3.cmd("route add -net 194.169.5.0/30 gw 194.169.8.1")
+	
+	#Routing R4
+	r4.cmd("sysctl net.ipv4.ip_forward=1")
+	r4.cmd("route add -net 194.169.1.0/30 gw 194.169.7.1")
+	r4.cmd("route add -net 194.169.6.0/30 gw 194.169.5.1")
+	r4.cmd("route add -net 194.169.3.0/30 gw 194.169.7.1")
+	r4.cmd("route add -net 194.169.8.0/30 gw 194.169.5.1")
+	r4.cmd("route add -net 194.169.2.0/30 gw 194.169.7.1")
+	
+	print("\nRouting telah selesai\n")
+	
+	#setting traffic dengan iperf
+	h2.cmd("iperf -s &")
+	h1.cmd("iperf -t 40 -B 194.169.1.1 -c 194.169.3.1 &")
+	h1.cmd("iperf -t 40 -B 194.169.6.1 -c 194.169.3.1 &")
+	
 	CLI(net)
 	net.stop()
 	
 if __name__=='__main__':
 	setLogLevel('info')
 	runTopo()
+	test_buffer = 'y'
+	while test_buffer == 'y':
+		runTopoBuffer()
+		test_buffer = input("Test buffer kembali? y/n   ")
